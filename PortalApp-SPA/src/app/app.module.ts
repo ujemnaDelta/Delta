62ba675befbddb21c3ee8255232233ebc699f31d
Gracjan <graku12@gmail.com>
2019-04-05 12:35:12 +0200

    Update Angular to 7.3.7 and add LoginComponent

--- PortalApp-SPA/src/app/app.module.ts
+++ PortalApp-SPA/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
 
+  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
+
+  MatToolbarModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule
+
+} from '@angular/material';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      LoginComponent
+   ],
+   imports: [
+      BrowserModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatCardModule,
+      MatInputModule,
+      MatDialogModule,
+      MatTableModule,
+      MatMenuModule,
+      MatIconModule,
+      MatProgressSpinnerModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
