0a14a5e5e11d1ebde67e478a7520f13f57d0e4eb
Tukowski <kofsonszymon@gmail.com>
2019-04-06 14:14:36 +0200

    Validation on UserName and Password, just required

--- PortalApp.API/DataTransferObjects/UserForRegisterDTO.cs
+++ PortalApp.API/DataTransferObjects/UserForRegisterDTO.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortalApp.API.DataTransferObjects
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string UserName { get; set; }       
+        [Required]
+        [StringLength(32, MinimumLength = 8, ErrorMessage="Password bettween 8 and 32")]
         public string UserPassword { get; set; }
     }
 }
